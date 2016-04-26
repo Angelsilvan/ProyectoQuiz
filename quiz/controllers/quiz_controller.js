6c2ab60f358abf49ba8978a1ebccddc4dcf4eba4
Angelsilvan <angelsilvan94@gmail.com>
2016-04-26 10:25:55 +0200

    Marco de Aplicación

--- quiz/controllers/quiz_controller.js
+++ quiz/controllers/quiz_controller.js
@@ -1,14 +1,15 @@
 // GET /quizes/question
 exports.question = function(req, res){
+	var answer = req.query.answer || '';	
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
 	if (req.query.respuesta === 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcto'});
+		res.render('quizes/answer', {respuesta: 'La respuesta es correcta'});
 	}
 	else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+		res.render('quizes/answer', {respuesta: 'La respuesta es incorrecta'});
 	}
 };
